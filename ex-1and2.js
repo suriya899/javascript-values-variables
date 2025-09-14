1651bee68966163bc28e08a7a1af9d8cda92a49c
Suriya Rupngam <ktaro112@gmail.com>
2025-09-14 23:04:54 +0700

    feat: enhance variable handling in ex-1and2.js, add Celsius to Fahrenheit conversion in ex-3.js, and clarify expressions in ex-4.js

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,29 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let firstName = "Karter";
+console.log(firstName);
+
+let lastName = "Anderson";
+console.log(lastName);
+
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+console.log(address);
+
+const restaurantName = "Chef's Table by Chef James";
+console.log(restaurantName);
+
+const netPrice = 15000;
+console.log(netPrice);
+
+const numberOfOpenRestaurants = 1500;
+console.log(numberOfOpenRestaurants);
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+console.log(address);
+
+netPrice = 5000;
+console.log(netPrice);
+
+numberOfOpenRestaurants = 2500;
+console.log(numberOfOpenRestaurants); 
+
